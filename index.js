ea45d9e470bb73c822e1ad0c0ab995eb74684678
sylvanou <sylvanoumbac24@gmail.com>
2017-11-29 18:50:56 -0500

    initial commit

---
+++ index.js
@@ -0,0 +1,54 @@
+// console.log("Ready for javascript")
+// console.log('Hello world!')
+
+var oper1 = 24 + 0
+var oper2 = 24 - 0
+var oper3 = 24 * 1
+var oper4 = 24 / 1
+// console.log(oper1)
+// console.log(oper2)
+// console.log(oper3)
+// console.log(oper4)
+
+var op1 = 6 % 3
+var op2 = 10 % 2
+var op3 = 5 % 2
+// console.log(op2 !== op3)
+// console.log(op1 > op2)
+
+var string1 = "Test Grades as follows:"
+var string2 = "Student 1:96"
+var string3 = "Comment:\"None\""
+
+// console.log(string1 + "\n\t" + string2 + "\n\t" + string3)
+
+var numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+// console.log(numbers[2] + numbers[4])
+
+// numbers += 10
+// console.log(numbers)
+
+var me = {
+    first: "Sylvano",
+    car: "Kia Forte",
+    jobs: ["Sandwich Artist", "Parking Attendant", "Developer"],
+    salary: 100000,
+    rich: true
+
+}
+
+// console.log(`My name is ${me.first}. I drive a ${me.car}. I have worked as a ${me.jobs}. I now make ${me.salary}. Am I rich? You could say it's ${me.rich}`)
+
+// var num = 1
+// while(num < 10) {
+//     if(num % 2 == 0){
+//         console.log(num)
+//     }
+//     num++
+// }
+
+for (var i = 1; i <= 10; i++){
+    if(i % 2 == 1){
+    console.log(i)
+    }
+}
